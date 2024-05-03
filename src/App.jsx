ce1df602e7f90d6d50d36a99bd0a06a69c77c7bc
Hunter Andersen <andersen.hunter42@gmail.com>
2024-05-03 00:00:30 -0700

    Working Simple Game

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,59 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 //For reference about glob imports: https://vitejs.dev/guide/features#glob-import
 const images = import.meta.glob("../images/*");
-console.log(images);
 const imagePaths = Object.keys(images);
 
 function App() {
   const [seenImages, setSeenImages] = useState([]);
+  const [memoryCount, setMemoryCount] = useState(0);
+  const [hasLost, setHasLost] = useState(false);
+
+  function validateMemory(userGuess) {
+    const hasSeenImage = seenImages.includes(currentImage);
+    if (!hasSeenImage) {
+      //Mark this image as having been seen
+      setSeenImages([...seenImages, currentImage]);
+    }
+
+    if (userGuess === hasSeenImage) {
+      setMemoryCount(memoryCount + 1);
+    } else {
+      setHasLost(true);
+    }
+  }
+
+  function restart(){
+    setSeenImages([]);
+    setMemoryCount(0);
+    setHasLost(false);
+  }
+
+  const currentImage =
+    imagePaths[Math.floor(Math.random() * imagePaths.length)];
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <ul>
-        {imagePaths.map((path, index) => {
-          return <li key={index}>
-            <img src={path} alt={`Memory image #${index}`} />
-          </li>
-        })}
-      </ul>
+      <h1>Test your Memory</h1>
+      <p>Correct Count: {memoryCount}</p>
+      {!hasLost ? (
+        <>
+          <div className="currentImage">
+            <img src={currentImage} alt="Current Image" />
+          </div>
+          <p>Have you seen this image previously</p>
+          <div className="userInput">
+            <button onClick={() => validateMemory(true)}>Yes</button>
+            <button onClick={() => validateMemory(false)}>No</button>
+          </div>
+        </>
+      ) : (
+        <>
+          <button onClick={restart}>Restart</button>
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
